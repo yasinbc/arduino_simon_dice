9421be7eea494da6893ae1dd68498d073a93c12d
atreus <atreus@nemesis.nemesis>
2023-01-11 01:08:06 +0100

    Descripcion del proyecto

--- simonDice.ino
+++ simonDice.ino
@@ -1,9 +1,15 @@
+/*
+    Autor: Yasin Boulahya Chairi
+    Asignatura: Tecnología de Computadores - UNIR
+    Actividad 2 Simon Dice en Arduino
+    Descripcion: Juego de Simon Dice en Arduino con 4 botones asociado a 4 leds
 
+*/
 
 void setup(){
 
 }
 
 void loop(){
-    
+
 }
